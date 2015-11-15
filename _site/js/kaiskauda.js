15cb780fb1333673362114dbad4c0568c06b7b35
Pijus Miega <miegamasis@gmail.com>
2015-11-15 06:36:51 +0200

    fixed-nav reenabled

--- _site/js/kaiskauda.js
+++ _site/js/kaiskauda.js
@@ -62,3 +62,25 @@ function addEventListenerLoop(elements, evnt, func) {
         elements[i].addEventListener(evnt, func);
     };
 };
+
+
+//Controls fixed nav when using desktop menu
+
+var fixedTrap;
+var headerState = 0;
+
+fixedTrap= document.querySelector("section").offsetTop;
+fixedNav();
+window.addEventListener("scroll", fixedNav);
+
+
+function fixedNav (){
+	var windowLoc = window.pageYOffset;
+	if ((windowLoc > fixedTrap) && (headerState === 0) ) {
+		document.getElementById('header-fixed').classList.toggle("active");
+		headerState = 1;
+	} else if((windowLoc < fixedTrap) && (headerState === 1) ){
+		document.getElementById('header-fixed').classList.toggle("active");
+		headerState = 0;
+	}
+}
